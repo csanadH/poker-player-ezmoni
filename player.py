f12084af4e145cf7e0b3086d144d749ff0f7df02
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-08-27 10:25:55 +0200

    Return minimum raise

--- player.py
+++ player.py
@@ -5,8 +5,9 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        print(game_state["pot"])        
-        return 1
+        print("RAISE")
+        print(game_state["current_buy_in"] - game_state["players[in_action][bet]"] + game_state["minimum_raise"]  )
+        return game_state["current_buy_in"] - game_state["players[in_action][bet]"] + game_state["minimum_raise"]     
 
     def showdown(self, game_state):
         pass
