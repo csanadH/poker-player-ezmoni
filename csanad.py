fff70cccf89402cd0af959d5d32e0c96db4c4481
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-08-27 13:52:46 +0200

    add print

--- csanad.py
+++ csanad.py
@@ -2,10 +2,11 @@ import json
 
 class Csanad:
     def myFunc(self, game_state):
-        print("CSANAD")
+        print("CSANAD START")
         try:
             hand = game_state["players"][game_state["in_action"]]["hole_cards"]
             print(hand)
+            print("CSANAD END")
             return
         except Exception as ex:
             print("bad csanad")
