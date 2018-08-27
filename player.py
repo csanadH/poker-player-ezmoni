c7fbbe6e0de2fa4cc0f3854d51501541f84f3c62
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-08-27 10:19:41 +0200

    Retry json parse

--- player.py
+++ player.py
@@ -1,10 +1,13 @@
 import json
+from pprint import pprint
 
 class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
         print("PARSED JSON")
+        parsedJson = json.load(game_state)
+        pprint(parsedJson)
         return 1
 
     def showdown(self, game_state):
