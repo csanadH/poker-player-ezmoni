b8d7860bb78d4f01058547e82e65f3d5f0816198
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-08-27 14:03:45 +0200

    Add new arguments

--- balint.py
+++ balint.py
@@ -1,3 +1,5 @@
+import json
+
 class Balint:
     def myFunc(self, game, hand, cards):
         print("Balint")
@@ -10,3 +12,10 @@ class Balint:
 
     def calcBet(self, game):
         return game["minimum_raise"] + 20
+
+    if __name__ == '__main__':
+        json_data=open("sample.json").read()
+        data = json.loads(json_data)
+        hole_cards = data["players"][data["in_action"]]["hole_cards"]
+        print(hole_cards)
+    
