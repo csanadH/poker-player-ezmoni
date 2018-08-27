01fbb414edafe0430f3fe901da5defdb0f19d5e2
Matt9993 <hriczmate@gmail.com>
2018-08-27 14:09:42 +0200

    exception handling added

--- mate.py
+++ mate.py
@@ -1,13 +1,17 @@
 
 class Mate:
     def isPair(self, game_state, hand, cards):
-        print(hand[0]["suit"])
-        first_card_rank = hand[0]["rank"]
-        second_card_rank = hand[1]["rank"]
-        if(first_card_rank == second_card_rank):
-            return 700
-        for card in cards:
-            if first_card_rank == second_card_rank == card["rank"]:
-                return 1000
-        else: 
-            return 0
+        try:
+            print(hand[0]["suit"])
+            first_card_rank = hand[0]["rank"]
+            second_card_rank = hand[1]["rank"]
+            if(first_card_rank == second_card_rank):
+                return 700
+            for card in cards:
+                if first_card_rank == second_card_rank == card["rank"]:
+                    return 1000
+            else: 
+                return 0
+            except Exception as ex:
+            print("bad mate")
+            print(ex)
