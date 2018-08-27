be727eab750f4e6b9c7769861833e1aa9810cd1b
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-08-27 13:48:29 +0200

    Print hand

--- csanad.py
+++ csanad.py
@@ -4,7 +4,8 @@ class Csanad:
     def myFunc(self, game_state):
         print("CSANAD")
         try:
-            print(game_state["players"][game_state["in_action"]])
+            hand = game_state["players"][game_state["in_action"]]["hole_cards"]
+            print(hand)
             return
         except Exception as ex:
             print("bad csanad")
