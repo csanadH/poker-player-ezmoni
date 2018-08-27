72f73516ebdc32a61a678c5c52a7cf09c9ac3309
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-08-27 14:04:46 +0200

    Print hand ranks

--- csanad.py
+++ csanad.py
@@ -4,6 +4,8 @@ class Csanad:
     def myFunc(self, game_state, hand, cards):
         print("CSANAD START")
         try:
+            print(hand[0]["rank"])
+            print(hand[1]["rank"])
             if (hand[0]["rank"] == hand[1]["rank"]):
                 print("pair")
             else:
