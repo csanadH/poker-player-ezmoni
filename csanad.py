7c53ee27f5d97bb88cf2059cdb77958c2387078a
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-08-27 12:01:56 +0200

    fix ex

--- csanad.py
+++ csanad.py
@@ -9,5 +9,5 @@ class Csanad:
             print("FIRST CARD=",hand[0]["rank"],",",hand[0]["suit"])
             print("SECOND CARD=",hand[1]["rank"],",",hand[1]["suit"])
         except Exception as ex:
-            print("bad csanad", end="")
+            print("bad csanad")
             print(ex)
