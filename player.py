fb07762353a81c4294761352194715ed07449c3c
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-08-27 10:33:32 +0200

    Add prints

--- player.py
+++ player.py
@@ -7,9 +7,12 @@ class Player:
     def betRequest(self, game_state):
         print("RAISE")
         players = game_state["in_action"]
+        print(players)
         bet = players["bet"]
+        print(bet)
+        print(game_state["community-cards"])
         print(game_state["current_buy_in"] - bet)
-        return game_state["current_buy_in"] - bet  
+        return 100
 
     def showdown(self, game_state):
         pass
