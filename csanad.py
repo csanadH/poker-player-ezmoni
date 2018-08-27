d441b5fb91cccc2d71ac0066fb293fb680495aee
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-08-27 11:59:13 +0200

    Fix exception concatenation

--- csanad.py
+++ csanad.py
@@ -6,9 +6,8 @@ class Csanad:
         try:
             parsed = json.loads(stringJSON)
             hand = parsed["players"][parsed["in_action"]]
-            # HAND[0] = first card
-            # HAND[1] = second card
             print("FIRST CARD=",hand[0]["rank"],",",hand[0]["suit"])
             print("SECOND CARD=",hand[1]["rank"],",",hand[1]["suit"])
         except Exception as ex:
-            print("bad csanad " + ex)
+            print("bad csanad", end="")
+            print(ex)
