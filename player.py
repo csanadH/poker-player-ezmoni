d51879202e80a9e1fcc86f0f54540d2bfa1f3841
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-08-27 10:27:32 +0200

    fix return value

--- player.py
+++ player.py
@@ -6,8 +6,8 @@ class Player:
 
     def betRequest(self, game_state):
         print("RAISE")
-        print(game_state["current_buy_in"] - game_state["players[in_action][bet]"] + game_state["minimum_raise"]  )
-        return game_state["current_buy_in"] - game_state["players[in_action][bet]"] + game_state["minimum_raise"]     
+        print(game_state["current_buy_in"] - game_state["players[in_action][bet]"])
+        return game_state["current_buy_in"] - game_state["players[in_action][bet]"]  
 
     def showdown(self, game_state):
         pass
