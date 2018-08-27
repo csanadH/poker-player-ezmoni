fd758aacd99cd517742515c789e790bdd5dcb5e9
Balint Csizmadia <balintcsizmadia1@gmail.com>
2018-08-27 14:18:22 +0200

    Temp addition

--- balint.py
+++ balint.py
@@ -1,20 +1,10 @@
-import json
-
 class Balint:
     def myFunc(self, game, hand, cards):
         print("Balint")
         try:
             print(hand)
             print(cards)
+            return 1000
         except:
             print("We have an exception in class Balint.")
-
-    def calcBet(self, game):
-        return game["minimum_raise"] + 20
-
-    if __name__ == '__main__':
-        json_data=open("sample.json").read()
-        data = json.loads(json_data)
-        hole_cards = data["players"][data["in_action"]]["hole_cards"]
-        print(hole_cards)
-    
+            return 0
