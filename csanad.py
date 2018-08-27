46f4e17449b5088eca45c257a42c1ae50336123d
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-08-27 11:56:46 +0200

    Fix prints

--- csanad.py
+++ csanad.py
@@ -8,7 +8,7 @@ class Csanad:
             hand = parsed["players"][parsed["in_action"]]
             # HAND[0] = first card
             # HAND[1] = second card
-            print("FIRST CARD="+hand[0]["rank"]+","+hand[0]["suit"])
-            print("SECOND CARD="+hand[1]["rank"]+","+hand[1]["suit"])
+            print("FIRST CARD=",hand[0]["rank"],",",hand[0]["suit"])
+            print("SECOND CARD=",hand[1]["rank"],",",hand[1]["suit"])
         except Exception as ex:
             print("bad csanad " + ex)
