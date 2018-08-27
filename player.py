38e0e158d8f8391b0c015bb7a80076e4246601d6
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-08-27 10:12:03 +0200

    Add print to see game_state

--- player.py
+++ player.py
@@ -3,6 +3,8 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
+        print("PRINT")
+        print(game_state)
         return 0
 
     def showdown(self, game_state):
